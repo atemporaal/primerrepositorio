bdcd3ae312a4f981731525b9f31ed3a2f46c6852
lucianoamaya <lucianoamaya@192.168.88.54>
2019-12-09 17:51:20 -0300

    mi primer conexion wn git hub

---
+++ primerrepostirorio/src/primerrepostirorio/Primerrepostirorio.java
@@ -0,0 +1,15 @@
+
+package primerrepostirorio;
+
+
+public class Primerrepostirorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" hola soy luciano");
+        
+    }
+    
+}
